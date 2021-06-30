5fcc19e1b7ce30426faf409ad7915ae22a569b4e
Sebastian <Sebas.10arango@gmail.com>
2021-06-29 22:21:17 -0500

    general.java añadido

---
+++ src/General.java
@@ -0,0 +1,31 @@
+public class General{
+    public static void main(String[] args){
+        getHelp();
+        notObject();
+        notPlace();
+        notWord();
+        commons();
+    }
+    
+    static void getHelp(){
+        System.out.println("Llegue vivo hasta el final de la historia y melo");//
+    }
+
+    static void notObject(){
+        System.out.println("¿Usted es atembao o que? Eso no existe, mentiroso");//
+    }
+
+    static void notPlace(){
+        System.out.println("Ira su mamá por allá");//
+    }
+
+    static void notWord(){
+        System.out.println("No tengo cochina idea de que sea eso");//
+    }
+
+    static void commons(){
+        System.out.println("Mera vuelta eso");//
+    }
+
+
+}
